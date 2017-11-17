8fee1b4acee816b2ecd983704beaca6faadc4cbc
Anton1237 <anton.pettersson@koping.net>
2017-11-17 13:19:23 +0100

    ny länk
    .

--- kod.js
+++ kod.js
@@ -8,4 +8,27 @@
             $(this).toggleClass("selected");    
         });
 
+    $("#bgcolor").change(function(){
+        $("body").css("background-color" , $(this).val());
+    
+    });
+    
+   
+     $("#textcolor").change(function(){
+        $("body").css("color" , $(this).val());
+    
+     });
+        
+        
+      $("#rubrik").focusout(function(){
+        $("h1").text( $(this).val());
+    
+     });   
+        
+      $("#rubrik").focusin(function(){
+        $("textarea").text("", $(this).val());
+    
+     });   
+        
+        
     }); 
