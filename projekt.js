31376f6cbb091690ecb331fb8cef71598d219ec3
Anton1237 <anton.pettersson@koping.net>
2017-12-15 11:47:13 +0100

    Klar!
    Hörshej

--- projekt.js
+++ projekt.js
@@ -1,15 +1,36 @@
 $(function(){
     $("#sakernav").click(function(){
         $("#saker").show();
+        $("#schysst").hide();
+        $("#standard").hide();
     });
 
-  $("#schysstnav").click(function(){
+    $("#schysstnav").click(function(){
         $("#schysst").show();
+        $("#standard").hide();
+        $("#saker").hide();
     });
 
-  $("#standardnav").click(function(){
+    $("#standardnav").click(function(){
         $("#standard").show();
+        $("#schysst").hide();
+        $("#saker").hide();
     });
 
+    $("#sakernav").click(function(){
+        $("#huvud").hide();
+       
+    });
+
+    $("#schysstnav").click(function(){
+        $("#huvud").hide();
+       
+    });
+
+
+    $("#standardnav").click(function(){
+        $("#huvud").hide();
+       
+    });
 
 });
